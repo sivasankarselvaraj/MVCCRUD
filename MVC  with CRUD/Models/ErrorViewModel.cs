b1729b8dbee440aa7f4516e450f65b35dd93abd9
Siva Sankar S <sivasankarselvarajv@gmail.com>
2023-12-25 12:02:28 +0530

    Add project files.

---
+++ MVC  with CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVC__with_CRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
